b0b66c27063e383c2745e48aedd14e2eb6a303e7
NasroAllah El Idrissi <nelidris@student.1337.ma>
2023-08-04 14:46:24 +0100

    botRemote

--- view/src/musicController/musicPlayer.tsx
+++ view/src/musicController/musicPlayer.tsx
@@ -23,11 +23,43 @@ const MusicSection = () => {
   );
 };
 
+const MusicQueueSong = () => {
+  return (
+    <div className="queue-song">
+      <div className="queue-song-image"></div>
+      <div className="queue-song-text">
+        <div className="queue-song-title">Music Title</div>
+        <div className="queue-song-author">Music Author</div>
+      </div>
+      <div className="queue-song-view">
+        <icons.Eye size="16" color="#636363" />
+        <p>123 456 789</p>
+      </div>
+      <div className="queue-song-duration">
+        <icons.Clock size="16" color="#636363" />
+        <p>00:00</p>
+      </div>
+    </div>
+  );
+};
+
+const MusicQueue = () => {
+  return (
+    <div className="music-queue">
+      <MusicQueueSong />
+      <MusicQueueSong />
+      <MusicQueueSong />
+      <MusicQueueSong />
+      <MusicQueueSong />
+    </div>
+  );
+};
+
 export const MusicPlayer = () => {
   return (
     <div className="music-player">
       <MusicSection />
-      <div className="music-queue">MusicQueue</div>
+      <MusicQueue />
     </div>
   );
 };
