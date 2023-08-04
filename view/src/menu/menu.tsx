a32c723d98cb77ea28c45216bea970f2ce1d8c2b
NasroAllah El Idrissi <nelidris@student.1337.ma>
2023-08-04 15:51:43 +0100

    botRemote

--- view/src/menu/menu.tsx
+++ view/src/menu/menu.tsx
@@ -1,17 +1,46 @@
+import { useState } from "react";
 import "./menu.css";
 import * as icons from "iconsax-react";
 
-const GuildSection = () => {
+const GuildDropdown = () => {
   return (
-    <div className="guild-select">
-      <div className="guild-picture"></div>
-      <div className="guild-name">Select a guild...</div>
-      <icons.ArrowDown2
-        size="20"
-        color="#636363"
-        className="guild-dropdown-icon"
-      />
+    <div className="guild-dropdown">
+      <div className="guild-dropdown-item">
+        <div className="guild-dropdown-item-image"></div>
+        <div className="guild-dropdown-item-name">Guild 1</div>
+      </div>
+      <div className="guild-dropdown-item">
+        <div className="guild-dropdown-item-image"></div>
+        <div className="guild-dropdown-item-name">Guild 2</div>
+      </div>
+      <div className="guild-dropdown-item">
+        <div className="guild-dropdown-item-image"></div>
+        <div className="guild-dropdown-item-name">Guild 3</div>
+      </div>
     </div>
+  );
+};
+
+const GuildSection = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const dropDownHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button className="guild-select" onClick={dropDownHandler}>
+        <div className="guild-picture"></div>
+        <div className="guild-name">Select a guild...</div>
+        <icons.ArrowDown2
+          size="20"
+          color="#636363"
+          className="guild-dropdown-icon"
+        />
+      </button>
+      {isOpen ? <GuildDropdown /> : null}
+    </>
   );
 };
 
